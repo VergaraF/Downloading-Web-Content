af2aa74d96108ccb92036db6d12adf3f756f5986
Fabian Vergara <vergarf@gmail.com>
2016-05-18 16:21:28 -0400

    Playing around

--- Downloading Web Content/ViewController.swift
+++ Downloading Web Content/ViewController.swift
@@ -10,8 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var webView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let url = NSURL(string: "http://www.stackoverflow.com")!
+        
+        /* /!\It downloads the whole website */
+        self.webView.loadRequest(NSURLRequest(URL: url))
+        
+        
+       /* /!\It downloads the HTML data from the website and displays it
+         
+         let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) in
+            // will happen when task completes (closure)
+            
+            if let urlContent = data{
+                
+                let webContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)
+                
+                dispatch_async(dispatch_get_main_queue(), {
+                    self.webView.loadHTMLString(String(webContent), baseURL: nil)
+                
+                })
+                
+                
+                print(webContent)
+                
+            }else{
+                //show error message
+            }
+        }
+        
+        task.resume()*/
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
